0eb69815c65794bc2a792e8be061e3525162e9b0
Vera O. Michael <veralynmichael@gmail.com>
2022-06-07 10:48:28 +0100

    new changes on admin page

--- cypress/fixtures/selectors.js
+++ cypress/fixtures/selectors.js
@@ -15,5 +15,30 @@ forgotYourPasswordBtn:"a > span"
 // }
 
 export const adminDashboard = {
-allApplicants:".MuiPaper-root > .MuiGrid-container > :nth-child(1)"
+allApplicants:".MuiPaper-root > .MuiGrid-container > :nth-child(1)",
+searchApplicants:".jss27 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input",
+searchText: "Alhassan",
+downloadIcon:".jss30 > :nth-child(1) > div > :nth-child(1) > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root",
+// exportCsv: ".MuiList-root > [tabindex="0"]"
+backHome: ".nav-menu > :nth-child(2) > a > .nav-text",
+allPayments: ":nth-child(2) > a > .jss44 > .MuiTypography-root",
+searchPayment: ".jss63 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input",
+paymentText: "Dolapo",
+completedApplicantions: ":nth-child(3) > a > .jss80 > .MuiTypography-root",
+searchApplications: ".jss99 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input",
+applicationText: "Talabi",
+noOfAdmission: ":nth-child(4) > a > .jss116 > .MuiTypography-root"
+
+
 }
+ export const userAccess = {
+userAccessText: ":nth-child(3) > .MuiButtonBase-root > .MuiButton-label",
+roles: ".open > .sub-menu > :nth-child(1) > .prepend-icon",
+userAccessSearch: ".MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input",
+userSearchText: "Director",
+addnewRoles: ".MuiButton-startIcon > .MuiSvgIcon-root > path",
+typenewRole: "MuiInputBase-input MuiInput-input",
+roleText: "Test Role",
+roleDescription: "MuiInputBase-input MuiInput-input",
+descriptionText: "Test Description"
+ }
--- cypress/integration/adminDashboard.js
+++ cypress/integration/adminDashboard.js
@@ -1,4 +1,4 @@
-import { adminSignIn,adminDashboard } from "../fixtures/selectors.js";
+import { adminSignIn,adminDashboard, userAccess } from "../fixtures/selectors.js";
  
 describe("Given that i'm on the School Admin Dashboard page", function(){
 
@@ -12,6 +12,47 @@ it("I should be able to login to navigate through different pages on the Admin P
     cy.get(adminSignIn.signInBtn).click()
     // cy.get(adminDashboard.pageTitle).should('be visible')
     // cy.get(adminDashboard.allApplicantsText).click()
+    
+})
+
+it("I should be able to click on all applicants icon and search for applicants", function(){
     cy.get(adminDashboard.allApplicants).click()
+    cy.get(adminDashboard.searchApplicants).type(adminDashboard.searchText)
+    cy.get(adminDashboard.backHome).click()
+})
+
+// it("I should be able to click and download list of all Applicants", function(){
+//     cy.get(adminDashboard.downloadIcon).click()
+//     cy.get(adminDashboard.backHome).click()
+// })
+
+it("I should be able to click on all Payments icon and search for all Payments", function(){
+    cy.get(adminDashboard.allPayments).click()
+    cy.get(adminDashboard.searchPayment).type(adminDashboard.paymentText)
+    cy.get(adminDashboard.backHome).click()
+})
+
+it("I should be able to click on all completed Applicantions and search for all Applcations", function(){
+    cy.get(adminDashboard.completedApplicantions).click()
+    cy.get(adminDashboard.searchApplications).type(adminDashboard.applicationText)
+    cy.get(adminDashboard.backHome).click()
 
 })
+
+it("I should be able to click on all admissions icon", function(){
+    cy.get(adminDashboard.noOfAdmission).click()
+    cy.get(adminDashboard.backHome).click()
+})
+ 
+it("I should be able to access pages under User Acess Module", function(){
+cy.get(userAccess.userAccessText).click()
+// cy.get(userAccess.userAccessSearch).type(userAccess.userSearchText)
+})
+
+it ("I should be able to click on Roles page", function(){
+    cy.get(userAccess.roles).click()
+    cy.get(userAccess.addnewRoles).click({force: true})
+    cy.get(userAccess.typenewRole).type(userAccess.roleText, {force: true})
+    cy.get(userAccess.roleDescription).type(userAccess.descriptionText, {force: true})
+})
+
